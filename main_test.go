57ee4ed4b90cdc2835984fe917d28cebb6d5f55b
Juan Molfese <juanjosemolfese@gmail.com>
2021-09-13 21:23:50 -0300

    Se agrega un segundo test que pasa por el else de la fun analiceChain

--- main_test.go
+++ main_test.go
@@ -12,4 +12,13 @@ func TestAnaliceChain(t *testing.T) {
 	if (c.Type == "CF" && c.Length == 4 && c.Value == "POLI" && err != nil) || (c.Type != "CF" || c.Length != 4 || c.Value != "POLI" && err == nil) {
 		t.Error("La funcion AnaliceChain no esta funcionando bien")
 	}
+
+}
+func TestAnaliceChainFalse(t *testing.T) {
+	c := model.NewChain("CF03POLI")
+	_, err := AnaliceChain(&c)
+	if (c.Type == "CF" && c.Length == 3 && c.Value == "POLI" && err == nil) || (c.Type != "CF" || c.Length != 3 || c.Value != "POLI" && err != nil) {
+		t.Error("La funcion AnaliceChain no esta funcionando bien")
+	}
+
 }
